219dd1aa65341a1a49b5b29abc4f89301b291744
Emanuele <egalli64@gmail.com>
2022-11-27 19:13:13 +0100

    val vs var

---
+++ src/main/scala/com/github/egalli64/s02/ValVar.scala
@@ -0,0 +1,27 @@
+/*
+  https://docs.scala-lang.org/scala3/book/taste-vars-data-types.html
+  https://github.com/egalli64/scala-playground
+
+  Variable declaration by var and val
+ */
+package com.github.egalli64.s02
+
+@main def valVar(): Unit =
+  // immutable
+  val a = 1
+  // mutable
+  var b = 2
+
+  println("a is " + a)
+  println("b is " + b)
+
+  // a += 1
+  b += 1
+  println("now b is " + b)
+
+  val msg1 = "Won't change"
+  var msg2 = "Could be reassigned"
+  println(msg1 + " - vs - " + msg2)
+  // msg1 = "Won't compile"
+  msg2 = "Reassigned to other string"
+  println(msg1 + " - vs - " + msg2)

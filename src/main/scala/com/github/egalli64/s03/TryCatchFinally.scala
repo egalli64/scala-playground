8ec199d9964104c9d5755fd8de1679d083228932
Emanuele <egalli64@gmail.com>
2022-12-30 12:21:43 +0100

    try catch finally

---
+++ src/main/scala/com/github/egalli64/s03/TryCatchFinally.scala
@@ -0,0 +1,16 @@
+/*
+  https://docs.scala-lang.org/scala3/book/taste-control-structures.html
+  https://github.com/egalli64/scala-playground
+
+  Control structures: try catch finally
+ */
+package com.github.egalli64.s03
+
+@main def tryCatchFinally(input: String): Unit =
+  try
+    val i: Int = input.toInt
+    println(s"Value is $i")
+  catch
+    case nfe: NumberFormatException => println("Can't get Int. " + nfe.getMessage)
+  finally
+    println("done")

1ee39585b6e641028e9574471dc2948424bca521
Emanuele <egalli64@gmail.com>
2023-01-25 12:06:58 +0100

    metal formatting review

--- src/main/scala/org/coursera/efpl/course1/week4/Lecture2.scala
+++ src/main/scala/org/coursera/efpl/course1/week4/Lecture2.scala
@@ -7,29 +7,30 @@
  */
 package org.coursera.efpl.course1.week4
 
+// A case class hierarchy
 trait Expression
 case class Number(n: Int) extends Expression
 case class Sum(e1: Expression, e2: Expression) extends Expression
 case class Product(e1: Expression, e2: Expression) extends Expression
 case class Var(name: String) extends Expression
 
-def eval(e: Expression): Int = e match //
-  case Number(n) => n
-  case Sum(e1, e2) => eval(e1) + eval(e2)
+def eval(e: Expression): Int = e match
+  case Number(n)       => n
+  case Sum(e1, e2)     => eval(e1) + eval(e2)
   case Product(e1, e2) => eval(e1) * eval(e2)
 
-def show(e: Expression): String = e match //
-  case Number(n) => n.toString
-  case Sum(e1, e2) => s"${show(e1)} + ${show(e2)}"
+def show(e: Expression): String = e match
+  case Number(n)       => n.toString
+  case Sum(e1, e2)     => s"${show(e1)} + ${show(e2)}"
   case Product(e1, e2) => s"${showPar(e1)} * ${showPar(e2)}"
-  case Var(name) => name
+  case Var(name)       => name
 
-def showPar(e: Expression): String = e match //
+def showPar(e: Expression): String = e match
   case e: Sum => s"(${show(e)})"
-  case _ => show(e)
+  case _      => show(e)
 
 @main
-def lecture2(): Unit = //
+def lecture2(): Unit =
   val e1 = Sum(Number(1), Number(1))
   println("Expression evaluated to: " + eval(e1))
   println("Showing expression: " + show(e1))

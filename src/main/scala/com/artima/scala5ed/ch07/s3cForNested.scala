bd0aa6fc14a9ec46ab2b1b864517110068970f0b
Emanuele <egalli64@gmail.com>
2023-05-13 10:41:57 +0200

    mid-stream variable

--- src/main/scala/com/artima/scala5ed/ch07/s3cForNested.scala
+++ src/main/scala/com/artima/scala5ed/ch07/s3cForNested.scala
@@ -17,14 +17,42 @@ def s3cForNested(): Unit =
   def fileLines(file: java.io.File) =
     scala.io.Source.fromFile(file).getLines().toArray
 
+  /**
+   * Nested loop example
+   *
+   * @param pattern
+   *   the searched pattern in files
+   */
   def grep(pattern: String): Unit =
     for
-      // outer loop on filesHere
+      // outer loop on filesHere on the "file" loop variable
       file <- filesHere if file.getName.endsWith(".sbt")
-      // inner loop on fileLines
+      // inner loop on fileLines on the "line" loop variable
       line <- fileLines(file) if line.trim.matches(pattern)
     do
-      // body of the (internal) loop
+      // body of the (internal) loop, uses the loop variables
       println(s"$file: ${line.trim}")
 
-  grep(".*scala.*")
+  /**
+   * Nested loop example, with a mid-stream variable binding
+   *
+   * @param pattern
+   *   the searched pattern in files
+   */
+  def grep2(pattern: String): Unit =
+    for
+      // filtered outer loop
+      file <- filesHere if file.getName.endsWith(".sbt")
+      // inner loop
+      line <- fileLines(file)
+      // define mid-stream variable "trimmed" before filtering
+      trimmed = line.trim if trimmed.matches(pattern)
+    do
+      // use a plain loop variable and the mid-stream one
+      println(s"$file: $trimmed")
+
+  println("*** Grep version 1 ***")
+  grep(".*scala-.*")
+
+  println("*** Grep version 2 ***")
+  grep2(".*scala-.*")

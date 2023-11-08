ff072e5365f92e1f8f2582b0b63bfa5720fa6d8a
Emanuele <egalli64@gmail.com>
2023-11-08 10:57:29 +0100

    function

---
+++ src/main/scala/com/horstmann/scala3ed/ch02/s07_function.scala
@@ -0,0 +1,76 @@
+/*
+Scala for the Impatient Third Edition - Cay S. Horstmann
+Source code shown in the book
+Download the zip file from: https://horstmann.com/scala/
+
+https://github.com/egalli64/scala-playground
+
+2. Control Structures and Functions
+ 7. Functions
+ */
+package com.horstmann.scala3ed.ch02
+
+@main
+def s07_function(): Unit =
+  /**
+   * A function
+   *
+   * @param x
+   *   the parameter
+   * @return
+   *   the result
+   */
+  def abs(x: Double) = if x >= 0 then x else -x
+
+  // invoking a method
+  val result = abs(-23.33)
+  println("The result of invoking the method is " + result)
+
+  /**
+   * A non-single-line function
+   *
+   * @param n
+   *   the parameter
+   * @return
+   *   the result
+   */
+  def iterativeFactorial(n: Int) =
+    var r = 1
+    for i <- 1 to n do r = r * i
+    // no "return" keyword, the return value is implicitly the last statement value
+    r
+  // optional tail, could make the code clearer
+  end iterativeFactorial
+
+  println("Factorial (iterative): " + iterativeFactorial(5))
+
+  /**
+   * The return type is mandatory for recursive functions
+   *
+   * @param n
+   *   the parameter
+   * @return
+   *   the result
+   */
+  def recursiveFactorial(n: Int): Int = if n <= 0 then 1 else n * recursiveFactorial(n - 1)
+
+  println("Factorial (recursive): " + recursiveFactorial(5))
+
+  /**
+   * If the last statement value in the code should be disregarded, the function could be tagged as
+   * of "Unit" type
+   *
+   * @param sb
+   *   a parameter (I/O)
+   * @param message
+   *   another parameter
+   */
+  def appender(sb: StringBuilder, message: String): Unit =
+    sb.append(java.time.Instant.now())
+    sb.append(": ")
+    sb.append(message)
+    sb.append("\n")
+
+  val sb = StringBuilder("Some text ... ")
+  appender(sb, "Hello")
+  println(sb)

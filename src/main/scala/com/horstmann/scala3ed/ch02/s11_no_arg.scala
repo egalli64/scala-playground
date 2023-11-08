4c8cb61e49bdb7a9aaacbfd24966e5ab1bb6a811
Emanuele <egalli64@gmail.com>
2023-11-08 16:17:48 +0100

    no arg function

---
+++ src/main/scala/com/horstmann/scala3ed/ch02/s11_no_arg.scala
@@ -0,0 +1,41 @@
+/*
+Scala for the Impatient Third Edition - Cay S. Horstmann
+Source code shown in the book
+Download the zip file from: https://horstmann.com/scala/
+
+https://github.com/egalli64/scala-playground
+
+2. Control Structures and Functions
+ 11. Functions without Parameters
+ */
+package com.horstmann.scala3ed.ch02
+
+@main
+def s11_no_arg(): Unit =
+  val data = List("a", "b", "c")
+
+  /**
+   * A function with no parameters - no parentheses, suggesting idempotence
+   *
+   * @return
+   *   the result
+   */
+  def myWords = data.mkString(",")
+  println("Calling idempotent function: " + myWords)
+  // won't compile with parentheses
+  // println(myWords())
+
+  // The call to mkString is eager, done at variable definition
+  val myWords2 = data.mkString(",")
+  println("Using a variable: " + myWords2)
+
+  /**
+   * Another function with no parameters - with parentheses, suggesting no idempotence
+   *
+   * @return
+   *   the result
+   */
+  def myWords3() = data.mkString(",")
+  println("Calling (non-idempotent) function: " + myWords3())
+  // won't compile without parentheses
+  // println(words2)

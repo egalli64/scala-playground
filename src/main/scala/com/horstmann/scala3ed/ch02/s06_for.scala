ce57609e87b1a8fe0173a2168b35833d8a75c286
Emanuele <egalli64@gmail.com>
2024-07-03 15:57:31 +0200

    clarify

--- src/main/scala/com/horstmann/scala3ed/ch02/s06_for.scala
+++ src/main/scala/com/horstmann/scala3ed/ch02/s06_for.scala
@@ -12,36 +12,39 @@ package com.horstmann.scala3ed.ch02
 
 @main
 def s06_for(): Unit =
+  println("The for loop is flexible")
+
   // a double for-loop
+  print("i in [1, 3] and j in [7, 9]: ")
   for
     i <- 1 to 3
-    j <- 1 to 3
+    j <- 7 to 9
   do print(f"${10 * i + j}%3d")
-
-  println("\n---")
+  println()
 
   // use a _guard_ to filter loops
+  print("i and j both in [1, 3] but i != j: ")
   for
     i <- 1 to 3
     j <- 1 to 3
     if i != j
   do print(f"${10 * i + j}%3d")
-
-  println("\n---")
+  println()
 
   // use _definition_ for complex loops
+  print("i in [1, 3], j in [4 - i, 3]: ")
+  // expecting 13 / 22, 23 / 31, 32, 33
   for
     i <- 1 to 3
     beg = 4 - i
     j <- beg to 3
-  do print(f"${10 * i + j}%3d")
-
-  println("\n---")
+  do print(f"($i) ${10 * i + j}%3d")
+  println()
 
   // use a _for comprehension_ to generate a collection of values
   val result = for i <- 1 to 10 yield i % 3
-  println("Values generated: " + result)
+  println("A for comprehension: " + result)
 
-  // the generated result is compatible with the generator
+  // the generated result is compatible with the generator: here, string to string
   val encoded = for c <- "Hello" yield (c + 1).toChar
-  println("Encoded string: " + encoded)
+  println("Encoded 'Hello' (plus 1): " + encoded)

0ad4a68652b3d93ffd22267b68a3387cb9b6d683
Emanuele <egalli64@gmail.com>
2024-07-03 12:41:30 +0200

    clarify

--- src/main/scala/com/horstmann/scala3ed/ch02/s05_loop.scala
+++ src/main/scala/com/horstmann/scala3ed/ch02/s05_loop.scala
@@ -15,13 +15,15 @@ def s05_loop(): Unit =
   var n = 6
   var r = 2
 
+  print("Plain while-do: ")
   while n > 1 do
     r *= n
     print(s"$n -> $r; ")
     n -= 1
   println()
 
-  // no do-while in Scala, but the while could be twisted in this way
+  // no do-while in Scala
+  print("Emulate do-while: ")
   n = 6
   r = 2
   while
@@ -29,23 +31,23 @@ def s05_loop(): Unit =
     print(s"$n -> $r; ")
     n -= 1
     n > 1
-  do () // All work was done in the condition block
+  do () // All work is done in the condition block
   println()
 
-  // no "classic" for-loop, just the "enhanced" one
+  // for-loop works on iterables (or sort of that) - here, on a range
   r = 2
   for i <- 2 to 6 do
     r *= i
     print(s"$i -> $r; ")
   println()
 
-  // Looping on chars in a string, close to a "classic" for-loop
+  // for-loop on a string
   val s = "Hello"
   var sum = 0
-  for i <- 0 until s.length do sum += s(i)
-  println(sum)
-
-  // Prefer using the "enhanced" for-loop
-  sum = 0
   for c <- "Hello" do sum += c
-  println(sum)
+  println("Adding up the chars in a string: " + sum)
+
+  // same for-loop on string, in a less Scala way
+  sum = 0
+  for i <- 0 until s.length do sum += s(i)
+  println("Same (muddier): " + sum)

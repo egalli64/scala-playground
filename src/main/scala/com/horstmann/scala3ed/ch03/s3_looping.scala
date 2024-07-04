f90b5da117f8642361e1e20174851d86f41ffc45
Emanuele <egalli64@gmail.com>
2024-07-04 17:05:20 +0200

    clarify

--- src/main/scala/com/horstmann/scala3ed/ch03/s3_looping.scala
+++ src/main/scala/com/horstmann/scala3ed/ch03/s3_looping.scala
@@ -12,26 +12,48 @@ package com.horstmann.scala3ed.ch03
 
 @main
 def s3_looping(): Unit =
+  // looping on array
+  //
   val a = Array(4, 5, 3)
-  val b = a.toBuffer
-  b ++= List(2, 7)
-  b.remove(0, 2)
+  println(s"An array: " + a.mkString(", "))
 
-  println("An array: " + a.mkString(", ") + " ")
-  // same as for i <- 0 until a.length _or_ 0 <- 0 to a.length -1 _or_ <- Range(0, a.length)
-  for i <- a.indices do println(s"a($i) is ${a(i)}")
-
-  println("An array buffer: " + b)
-  for i <- b.indices do println(s"b($i) is ${b(i)}")
-
-  print("When the index is not important: ")
-  for x <- b do print(x + " ")
+  print("Looping on its values: ")
+  for x <- a do print(x + " ")
   println()
 
-  print("The array from right to left [ ")
+  print("Looping on its indices: ")
+  for i <- a.indices do print(s"a($i) is ${a(i)} ")
+  println()
+
+  print("... from right to left [ ")
   for i <- a.indices.reverse do print(s"a($i)=${a(i)} ")
   println("]")
 
   print("Same, less readable [ ")
   for i <- a.length - 1 to 0 by -1 do print(s"a($i)=${a(i)} ")
+  println("]")
+
+  print("Looping on range in [0, len): ")
+  // or: i <- 0 until a.length
+  // or: 0 to a.length -1
+  for i <- Range(0, a.length) do print(s"a($i) is ${a(i)} ")
+  println("\n")
+
+  // looping on array buffer
+  //
+  val b = a.toBuffer
+  b ++= List(2, 7)
+  b.remove(0, 2)
+  println("An array buffer: " + b)
+
+  print("Looping on its values: ")
+  for x <- b do print(x + " ")
+  println()
+
+  print("Looping on its indices: ")
+  for i <- b.indices do print(s"b($i) is ${b(i)} ")
+  println()
+
+  print("... from right to left [ ")
+  for i <- b.indices.reverse do print(s"b($i)=${b(i)} ")
   println("]")

c8fb651f8dfb91a61795ee60a4dcd0b395b99b3a
Emanuele <egalli64@gmail.com>
2024-07-04 16:18:16 +0200

    clarify

--- src/main/scala/com/horstmann/scala3ed/ch03/s2_array_buffer.scala
+++ src/main/scala/com/horstmann/scala3ed/ch03/s2_array_buffer.scala
@@ -6,7 +6,7 @@ Download the zip file from: https://horstmann.com/scala/
 https://github.com/egalli64/scala-playground
 
 3. Working with Arrays
- 2. Variable-Length Arrays: Array Buffers - ArrayBuffer equivalent to Java ArrayList
+ 2. Variable-Length Arrays: Array Buffers - ArrayBuffer is equivalent to Java ArrayList
  */
 package com.horstmann.scala3ed.ch03
 
@@ -15,7 +15,7 @@ import scala.collection.mutable.ArrayBuffer
 @main
 def s2_array_buffer(): Unit =
   val b = ArrayBuffer[Int]()
-  println("An empty integer array buffer: " + b)
+  println(s"An empty int array buffer: " + b)
 
   b += 1
   println("After appending 1 - O(1): " + b)
@@ -30,7 +30,7 @@ def s2_array_buffer(): Unit =
   println("After inserting a 6 in position 2 - O(n): " + b)
 
   b.insertAll(2, Array(7, 8, 9))
-  println("After inserting a collection in position 2 - O(n): " + b)
+  println("After inserting an iterable in position 2 - O(n): " + b)
 
   b.remove(2)
   println("After removing the element in position 2 - O(n): " + b)
@@ -38,8 +38,10 @@ def s2_array_buffer(): Unit =
   b.remove(2, 3)
   println("After removing 3 elements from position 2 on - O(n): " + b)
 
-  // using ArrayBuffer as an array builder
+  // using ArrayBuffer as a raw array builder
   val a = b.toArray
+  println(s"The newly created array, ${a.getClass.getName}, is: " + a.mkString(", "))
+
   b += 42
-  println("The newly created array is: " + a.mkString(", "))
   println("The array buffer now is: " + b)
+  println("Changes on the buffer do not reflect on the raw array: " + a.mkString(", "))

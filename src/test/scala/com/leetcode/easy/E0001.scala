0fc361d1722a819e5525f2954a7e9eaae8c457c4
Emanuele <egalli64@gmail.com>
2024-07-08 17:26:21 +0200

    minor cleanup

--- src/test/scala/com/leetcode/easy/E0001.scala
+++ src/test/scala/com/leetcode/easy/E0001.scala
@@ -11,13 +11,13 @@ import org.scalatest.matchers.should.Matchers
 
 class E0001 extends AnyFunSuite with Matchers:
   test("Example 1") {
-    twoSum(Array(2,7,11,15), 9) shouldBe Array(0,1)
+    twoSum(Array(2, 7, 11, 15), 9) shouldBe Array(0, 1)
   }
 
   test("Example 2") {
-    twoSum(Array(3,2,4), 6) shouldBe Array(1,2)
+    twoSum(Array(3, 2, 4), 6) shouldBe Array(1, 2)
   }
 
   test("Example 3") {
-    twoSum(Array(3,3), 6) shouldBe Array(0,1)
+    twoSum(Array(3, 3), 6) shouldBe Array(0, 1)
   }
